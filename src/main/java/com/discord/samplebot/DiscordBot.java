f5881c9f3252719a97e79bb33cd86c0106847716
nndoi <d-naoki51@outlook.jp>
2024-10-22 18:25:37 +0900

    change global command to guild command
    global commandがdiscord botに反映される時間は長いため、開発中はguildを用いる

--- src/main/java/com/discord/samplebot/DiscordBot.java
+++ src/main/java/com/discord/samplebot/DiscordBot.java
@@ -4,6 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -15,22 +16,47 @@ public class DiscordBot extends ListenerAdapter {
 	private static JDA jda = null;
 	public static Dotenv dotenv = Dotenv.load();
 	private static final String BOT_TOKEN = dotenv.get("DISCORD_API");
+	private static final String GUILD_ID = dotenv.get("GUILD_ID");
+	private static final String MODE = "dev";
 
 	public static void main(String[] args) {
 		jda = JDABuilder.createDefault(BOT_TOKEN)
                 .setRawEventsEnabled(true)
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(new DiscordBot())
-                .setActivity(Activity.playing("プログラミ"))
+                .setActivity(Activity.playing("プログラミング"))
                 .build();
 		
-		jda.updateCommands()
-		.addCommands(Commands.slash("register", "register member")
-				.addOption(OptionType.STRING, "username", "register username", true))
-		.addCommands(Commands.slash("delete", "delete member")
-				.addOption(OptionType.STRING, "username", "delete username", true))
-		.addCommands(Commands.slash("show", "delete member"))
-		.queue();
+		try {
+			jda.awaitReady();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		if (MODE.equals("dev")) {
+			// 開発環境
+			Guild guild = jda.getGuildById(GUILD_ID);
+			
+			guild.updateCommands()
+			.addCommands(Commands.slash("register", "register member")
+					.addOption(OptionType.STRING, "username", "register username", true))
+			.addCommands(Commands.slash("delete", "delete member")
+					.addOption(OptionType.STRING, "username", "delete username", true))
+			.addCommands(Commands.slash("show", "delete member"))
+			.queue();
+		}
+		else {
+			// 本番環境
+			jda.updateCommands()
+			.addCommands(Commands.slash("register", "register member")
+					.addOption(OptionType.STRING, "username", "register username", true))
+			.addCommands(Commands.slash("delete", "delete member")
+					.addOption(OptionType.STRING, "username", "delete username", true))
+			.addCommands(Commands.slash("show", "delete member"))
+			.queue();
+		}
+		
+		
 	
 
 	}
