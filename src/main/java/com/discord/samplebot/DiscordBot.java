5ad59998eb9eeb2a4263844110b1785cd69e34d5
nndoi <d-naoki51@outlook.jp>
2024-10-26 01:46:22 +0900

    feat: connection to valorant server

--- src/main/java/com/discord/samplebot/DiscordBot.java
+++ src/main/java/com/discord/samplebot/DiscordBot.java
@@ -1,5 +1,10 @@
 package com.discord.samplebot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -19,6 +24,7 @@ public class DiscordBot extends ListenerAdapter {
 	public static Dotenv dotenv = Dotenv.load();
 	private static final String BOT_TOKEN = dotenv.get("DISCORD_API");
 	private static final String GUILD_ID = dotenv.get("GUILD_ID");
+	private static final String VALORANT_ID = dotenv.get("VALORANT_ID");
 	private static final String MODE = "dev";
 	private static final Member members = new Member();
 
@@ -46,6 +52,7 @@ public class DiscordBot extends ListenerAdapter {
 			.addCommands(Commands.slash("delete", "delete member")
 					.addOption(OptionType.STRING, "username", "delete username", true))
 			.addCommands(Commands.slash("show", "delete member"))
+			.addCommands(Commands.slash("get", "get match informantion"))
 			.queue();
 		}
 		else {
@@ -59,9 +66,42 @@ public class DiscordBot extends ListenerAdapter {
 			.queue();
 		}
 		
-		
+	}
 	
+	public void checkValorantConnection() throws IOException{
+		URL baseURL = new URL(dotenv.get("VALORANT_URL"));
+		HttpURLConnection connection = (HttpURLConnection) baseURL.openConnection();
+        connection.setRequestProperty("Authorization", VALORANT_ID);
+ 
+        connection.setRequestMethod("GET");
+        System.out.println(connection.getRequestProperties());
+        //通信開始
+        connection.connect();
 
+        switch (connection.getResponseCode()) {
+            case 200:
+                break;
+            case 403:
+            	System.out.println("403");
+                break;
+            case 404:
+            	System.out.println("404");
+            	break;
+            case 429:
+            	System.out.println("429");
+            	break;
+            default:
+            	System.out.println("another");
+            	break;
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String msg;
+
+        while ((msg = reader.readLine()) != null) {
+            System.out.println(msg);
+        }
+        
 	}
 	
 	//メッセージの反応メソッド
@@ -91,6 +131,15 @@ public class DiscordBot extends ListenerAdapter {
 			List<String> memberList = members.getMembers();
 			replyString = "メンバーリスト\n" + String.join("\n", memberList);
 			System.out.print("show");
+		}
+		else if (event.getName().equals("get")) {
+			try {
+				checkValorantConnection();
+			} catch (IOException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+			replyString = "get command が入力されました";
 		}
 		else { 
 			replyString = "該当するコマンドがありません";
